720f82a2bc0858f82adac807f76f9734844dbc7a
arnabd88 <arnabd@lab1-19.eng.utah.edu>
2016-11-14 17:46:56 -0700

    Porting from cade

--- run.sh
+++ run.sh
@@ -9,3 +9,5 @@ python Assignment5_SVM.py -fold 1 handwriting/train.data handwriting/train.label
 python Assignment5_SVM.py -fold 5 madelon/madelon_train.data madelon/madelon_train.labels -test madelon/madelon_test.data madelon/madelon_test.labels > Q3_2
 
 python Assignment_DTs.py -fold 1 handwriting/train.data handwriting/train.labels -test handwriting/test.data handwriting/test.labels > Q3_Ensemble_discrete
+
+python Assignment_cont_DTs.py -fold 1 madelon/madelon_train.data madelon/madelon_train.labels -test madelon/madelon_test.data madelon/madelon_test.labels > Q3_Ensemble_continuous
